36994844c341c943192d7e4b1d56dc1a071e1c7c
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-03-02 10:32:36 +0100

    Ajout fichiers Bootstrap en local

--- view/partials/head.php
+++ view/partials/head.php
@@ -4,10 +4,13 @@
     
     <title>Gestion des relevés d'heures</title>
     
+    <!-- Chargement des fichiers Bootstrap en local -->
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.css"/>
     <!-- CDN pour charger Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
+    
     <!-- CDN pour charger Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     
     <!-- Toujours mettre la feuille de style en dernière position ! -->
     <link rel="stylesheet" href="public/css/style.css" />
