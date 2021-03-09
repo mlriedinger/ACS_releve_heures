f654a2895ad0cedcf67bda8275d41a669b559324
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-03-09 16:30:40 +0100

    Modif toast alert

--- view/partials/toastAlert.php
+++ view/partials/toastAlert.php
@@ -2,7 +2,7 @@
   <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000" data-bs-animation="true">
     <div class="toast-header bg-dark text-white">
       <strong class="me-auto">Notification</strong>
-      <small>11 mins ago</small>
+      <!-- <small>11 mins ago</small> -->
       <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
     <div class="toast-body">
