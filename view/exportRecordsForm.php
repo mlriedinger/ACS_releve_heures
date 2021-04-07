35008d488f511f4730e0a2ad009aaba2a926c4c5
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-04-07 16:18:36 +0200

    feature(export): visual modification export form

--- view/exportRecordsForm.php
+++ view/exportRecordsForm.php
@@ -17,71 +17,32 @@
 
                             <p class="fs-5 text fw-light mb-3">Quel(s) type(s) de relevés souhaitez-vous exporter ?</p>
 
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <div class="input-group-text">
                                     <input class="form-check-input mt-0" type="radio" name="scope" value="all" aria-label="Bouton radio permettant de sélectionner tous les relevés" checked />
                                 </div>
                                 <input type="text" class="form-control" aria-label="Sélectionner tous les relevés" value="Tous les relevés (par défaut)"/>
                             </div>
 
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <div class="input-group-text">
                                     <input class="form-check-input mt-0" type="radio" name="scope" value="valid" aria-label="Bouton radio permettant de sélectionner tous les relevés" />
                                 </div>
                                 <input type="text" class="form-control" aria-label="Sélectionner tous les relevés" value="Uniquement les relevés &#34validés&#34"/>
                             </div>
 
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <div class="input-group-text">
                                     <input class="form-check-input mt-0" type="radio" name="scope" value="unchecked" aria-label="Bouton radio permettant de sélectionner tous les relevés" />
                                 </div>
                                 <input type="text" class="form-control" aria-label="Sélectionner tous les relevés" value="Uniquement les relevés &#34en attente&#34"/>
                             </div>
 
-                            <div class="input-group mb-1">
+                            <div class="input-group mb-2">
                                 <div class="input-group-text">
                                     <input class="form-check-input mt-0" type="radio" name="scope" value="deleted" aria-label="Bouton radio permettant de sélectionner tous les relevés" />
                                 </div>
                                 <input type="text" class="form-control" aria-label="Sélectionner tous les relevés" value="Uniquement les relevés supprimés"/>
-                            </div>
-
-                        </div>
-                    
-                    </div>
-
-
-                    <div class="row mt-5 mb-3 justify-content-md-center">   
-
-                        <div class="col-sm-6">
-
-                            <p class="fs-5 text fw-light mb-3">Souhaitez-vous sélectionner une période ? (facultatif)</p>
-
-                            <div class="accordion" id="exportFormAccordion">
-                                <div class="accordion-item">
-
-                                    <h2 class="accordion-header" id="headingOne">
-                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                            Cliquez ici pour sélectionner une période.
-                                        </button>
-                                    </h2>
-
-                                    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#exportFormAccordion">
-
-                                        <div class="accordion-body">
-                                            <div class="input-group mb-3">
-                                                <span class="input-group-text" id="periodStart_selector">Du</span>
-                                                <input type="date" name="periodStart" id="periodStart" class="form-control" aria-label="Sélectionnez une date de début" aria-describedby="periodStart_selector"/>
-                                            </div>
-
-                                            <div class="input-group mb-3">
-                                                    <span class="input-group-text" id="periodEnd_selector">Au</span>
-                                                    <input type="date" name="periodEnd" id="periodEnd" class="form-control" aria-label="Sélectionnez une date de fin" aria-describedby="periodEnd_selector"/>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-
                             </div>
 
                         </div>
@@ -92,28 +53,37 @@
 
                         <div class="col-sm-6">
 
-                            <p class="fs-5 text fw-light mb-3">Souhaitez-vous sélectionner d'autres options ? (facultatif)</p>
-
                             <div class="accordion" id="exportFormAccordion">
                                 <div class="accordion-item">
 
                                     <h2 class="accordion-header" id="headingOne">
                                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseOne">
-                                            Cliquez ici pour sélectionner un manager ou un utilisateur.
+                                        Souhaitez-vous sélectionner d'autres options ? (facultatif)
                                         </button>
                                     </h2>
 
                                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#exportFormAccordion">
 
                                         <div class="accordion-body">
-                                            <label for="selectManager" class="mb-2">Exporter les relevés de tous les salariés liés à un manager </label>
+                                            <label for="periodStart_selector">Sélectionnez une période :</label>
+                                            <div class="input-group mt-4 mb-3">
+                                                <span class="input-group-text" id="periodStart_selector">Du</span>
+                                                <input type="date" name="periodStart" id="periodStart" class="form-control" aria-label="Sélectionnez une date de début" aria-describedby="periodStart_selector"/>
+                                            </div>
+                                            
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text" id="periodEnd_selector">Au</span>
+                                                <input type="date" name="periodEnd" id="periodEnd" class="form-control" aria-label="Sélectionnez une date de fin" aria-describedby="periodEnd_selector"/>
+                                            </div>
+
+                                            <label for="selectManager" class="mt-4 mb-2">Exporter les relevés de tous les salariés liés à un manager </label>
                                             <select name="manager" id="selectManager" class="form-select mb-3" aria-label="Default select example">
-                                                <option value ="" selected>Sélectionner un manager</option>
+                                                <option value ="" selected>Sélectionnez un manager</option>
                                             </select>
 
                                             <label for="selectUser" class="mt-4 mb-2">Exporter les relevés d'un salarié en particulier </label>
                                             <select name="user" id="selectUser" class="form-select mb-3" aria-label="Default select example">
-                                                <option value ="" selected>Sélectionner un salarié</option>
+                                                <option value ="" selected>Sélectionnez un salarié</option>
                                             </select>
                                         </div>
 
