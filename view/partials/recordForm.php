6646c4c73b5f36539380d11b178b833810b5da10
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-04-22 17:11:00 +0200

    feat(add new record): fix input display according to settings

--- view/partials/recordForm.php
+++ view/partials/recordForm.php
@@ -13,82 +13,97 @@
             </select>
         </div>
 
-        <div class="col flex-shrink-1 mb-3" <?= $_SESSION['dateTimeMgmt'] == 1 ? "hidden" : ""; ?> >
-            <span class="input-group-text" id="worksite_selector">Date</span>
-            <input type="date" name="recordDate" id="recordDate" class="form-control" aria-label="Sélectionnez une date" aria-describedby="date_selector" />
-        </div>
+        <?php 
+        if($_SESSION['lengthMgmt'] == 1){ ?>
+            <div class="col flex-shrink-1 mb-3">
+                <span class="input-group-text" id="worksite_selector">Date</span>
+                <input type="date" name="recordDate" id="recordDate" class="form-control" aria-label="Sélectionnez une date" aria-describedby="date_selector" required/>
+            </div>
+        <?php } ?>
 
     </div>
 
 
-    <!-- Champs pour un relevé avec date et heure de début / date et heure de fin -->
-    <div id="divWorkDateTimeInput" class="row mb-3 justify-content-md-center" <?= $_SESSION['dateTimeMgmt'] == 0 ? "hidden" : ""; ?> >
+    <?php 
+    if($_SESSION['dateTimeMgmt'] == 1){ ?>
+        <!-- Champs pour un relevé avec date et heure de début / date et heure de fin -->
+        <div id="divWorkDateTimeInput" class="row mb-3 justify-content-md-center">
 
-        <p class="h6 text-center mb-3">Temps de travail</p>
+            <p class="h6 text-center mb-3">Temps de travail</p>
 
-        <div class="col mb-3">
-            <span class="input-group-text" id="datetime_start_selector">Début</span>
-            <input type="datetime-local" name="datetimeStart" id="datetime_start" class="form-control" aria-label="Sélectionnez une date et une heure de début" aria-describedby="datetime_start_selector" />
+            <div class="col mb-3">
+                <span class="input-group-text" id="datetime_start_selector">Début</span>
+                <input type="datetime-local" name="datetimeStart" id="datetime_start" class="form-control" aria-label="Sélectionnez une date et une heure de début" aria-describedby="datetime_start_selector" required/>
+            </div>
+
+            <div class="col mb-3">
+                <span class="input-group-text" id="datetime_end_selector">Fin</span>
+                <input type="datetime-local" name="datetimeEnd" id="datetime_end" class="form-control" aria-label="Sélectionnez une date et une heure de fin" aria-describedby="datetime_end_selector" required/>
+            </div>
+
         </div>
+    <?php } ?>
+    
 
-        <div class="col mb-3">
-            <span class="input-group-text" id="datetime_end_selector">Fin</span>
-            <input type="datetime-local" name="datetimeEnd" id="datetime_end" class="form-control" aria-label="Sélectionnez une date et une heure de fin" aria-describedby="datetime_end_selector" />
+    <?php 
+    if($_SESSION['lengthMgmt'] == 1) { ?>
+        <!-- Champs pour un relevé avec seulement une durée -->
+        <div id="divWorkLengthInput" class="row mb-3 justify-content-md-center">
+
+            <p class="h6 text-center mb-3">Temps de travail</p>
+
+            <div class="col mb-3">
+                <span class="input-group-text" id="work_hours_indicator">Heures</span>
+                <input type="number" min="-15" name="workLengthHours" id="workLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="work_hours_indicator" required/>
+            </div>
+
+            <div class="col mb-3">
+                <span class="input-group-text" id="work_minutes_indicator">Minutes</span>
+                <input type="number" min="-15" step="15" max="60" name="workLengthMinutes" value="0" id="workLengthMinutes" onclick="incrementHour(workLengthHours, workLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="work_minutes_indicator" required/>
+            </div>
+
         </div>
+    <?php } ?>
 
-    </div>
+    <?php 
+    if($_SESSION['breakMgmt'] == 1){ ?>
+        <!-- Champs pour un relevé avec gestion du temps de pause -->
+        <div id="divBreakTime" class="row mb-3 justify-content-md-center">
 
-    <!-- Champs pour un relevé avec seulement une durée -->
-    <div id="divWorkLengthInput" class="row mb-3 justify-content-md-center" <?= $_SESSION['lengthMgmt'] == 0 ? "hidden" : ""; ?> >
+            <p class="h6 text-center mb-3">Temps de pause</p>
 
-        <p class="h6 text-center mb-3">Temps de travail</p>
+                <div class="col mb-3">
+                    <span class="input-group-text" id="trip_hours_indicator">Heures</span>
+                    <input type="number" min="-15" name="breakLengthHours" id="breakLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="trip_hours_indicator" required/>
+                </div>
 
-        <div class="col mb-3">
-            <span class="input-group-text" id="work_hours_indicator">Heures</span>
-            <input type="number" min="-15" name="workLengthHours" id="workLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="work_hours_indicator" />
+                <div class="col mb-3">
+                    <span class="input-group-text" id="trip_minutes_indicator">Minutes</span>
+                    <input type="number" min="-15" step="15" max="60" name="breakLengthMinutes" id="breakLengthMinutes" value="0" onclick="incrementHour(breakLengthHours, breakLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="trip_minutes_indicator" required/>
+                </div>
+
         </div>
+    <?php } ?>
 
-        <div class="col mb-3">
-            <span class="input-group-text" id="work_minutes_indicator">Minutes</span>
-            <input type="number" min="-15" step="15" max="60" name="workLengthMinutes" value="0" id="workLengthMinutes" onclick="incrementHour(workLengthHours, workLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="work_minutes_indicator" />
-        </div>
+    <?php 
+    if($_SESSION['tripMgmt'] == 1) { ?>
+        <!-- Champs pour un relevé avec gestion du temps de trajet -->
+        <div id="divTripTimeInput" class="row mb-3 justify-content-md-center" <?= $_SESSION['tripMgmt'] == 0 ? "hidden" : ""; ?> >
 
-    </div>
-
-    <!-- Champs pour un relevé avec gestion du temps de pause -->
-    <div id="divBreakTime" class="row mb-3 justify-content-md-center" <?= $_SESSION['breakMgmt'] == 0 ? "hidden" : ""; ?> >
-
-        <p class="h6 text-center mb-3">Temps de pause</p>
+            <p class="h6 text-center mb-3">Temps de trajet</p>
 
             <div class="col mb-3">
                 <span class="input-group-text" id="trip_hours_indicator">Heures</span>
-                <input type="number" min="-15" name="breakLengthHours" id="breakLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="trip_hours_indicator" />
+                <input type="number" min="-15" name="tripLengthHours" id="tripLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="trip_hours_indicator" />
             </div>
 
             <div class="col mb-3">
                 <span class="input-group-text" id="trip_minutes_indicator">Minutes</span>
-                <input type="number" min="-15" step="15" max="60" name="breakLengthMinutes" id="breakLengthMinutes" value="0" onclick="incrementHour(breakLengthHours, breakLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="trip_minutes_indicator" />
+                <input type="number" min="-15" step="15" max="60" name="tripLengthMinutes" id="tripLengthMinutes" value="0" onclick="incrementHour(tripLengthHours, tripLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="trip_minutes_indicator" />
             </div>
 
-
-    </div>
-
-    <!-- Champs pour un relevé avec gestion du temps de trajet -->
-    <div id="divTripTimeInput" class="row mb-3 justify-content-md-center" <?= $_SESSION['tripMgmt'] == 0 ? "hidden" : ""; ?> >
-
-        <p class="h6 text-center mb-3">Temps de trajet</p>
-
-        <div class="col mb-3">
-            <span class="input-group-text" id="trip_hours_indicator">Heures</span>
-            <input type="number" min="-15" name="tripLengthHours" id="tripLengthHours" value="0" class="form-control" aria-label="Indiquez le nombre d'heures de trajet" aria-describedby="trip_hours_indicator" />
         </div>
-
-        <div class="col mb-3">
-            <span class="input-group-text" id="trip_minutes_indicator">Minutes</span>
-            <input type="number" min="-15" step="15" max="60" name="tripLengthMinutes" id="tripLengthMinutes" value="0" onclick="incrementHour(tripLengthHours, tripLengthMinutes)" class="form-control" aria-label="Indiquez le nombre de minutes de trajet" aria-describedby="trip_minutes_indicator" />
-        </div>
-
-    </div>
+    <?php } ?>
 
     <!-- Champs commentaire -->
     <div id="divCommentFieldInput" class="row mb-3 justify-content-md-center">
