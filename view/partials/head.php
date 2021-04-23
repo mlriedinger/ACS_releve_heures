c34659c374b8f06e8b78d2c1164f0b762402502e
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-04-23 13:56:17 +0200

    css: delete unnecessary bootstrap files

--- view/partials/head.php
+++ view/partials/head.php
@@ -5,7 +5,7 @@
     <title>Gestion des relevés d'heures</title>
     
     <!-- Chargement des fichiers Bootstrap en local -->
-    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.css"/>
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css"/>
     <!-- CDN pour charger Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
     
