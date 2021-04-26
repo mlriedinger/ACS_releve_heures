e5218bb389a7ef2660bdc8779499c2fb6c807a6a
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-04-26 09:20:19 +0200

    fix partials/head suppression

--- view/login.php
+++ view/login.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-    <?php include('partials/head.php'); ?>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+        
+        <title> <?= $title ?> </title>
+        
+        <!-- Chargement des fichiers Bootstrap en local -->
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css"/>
+        <!-- CDN pour charger Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"/>
+        
+        <!-- CDN pour charger Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+        
+        <!-- Toujours mettre la feuille de style en dernière position ! -->
+        <link rel="stylesheet" href="public/css/style.css" />
+    </head>
 
     <body>
         <div class="container">
