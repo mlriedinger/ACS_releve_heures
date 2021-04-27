ac6b6ac57263b60a36632536d5db0112967669a8
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-04-27 16:40:41 +0200

    feat(home): add test content

--- view/home.php
+++ view/home.php
@@ -1,42 +1,43 @@
 <?php 
 
 $title = "Accueil";
-$heading = "Chantiers du jour";
+$heading = "Accueil<br/>(à personnaliser)";
 
 ?>
 
 <?php ob_start(); ?>
+    <div class="row mb-5">
+        <div class="col mt-5">
+            <img src="<?= $_SESSION['imgFilePath']. "illustration.svg"?>" alt="Illustration d'organisation" height="300">
+        </div>
+        <div class="col mt-5">
+            <p class="fs-3">Aliquam gravida egestas eleifend.</p>
+            <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget molestie eros. Aliquam sed enim eget leo lacinia pretium id sit amet mauris. Aenean sed pharetra massa. 
+                Vestibulum nibh erat, accumsan vitae elit in, semper commodo ipsum. Phasellus rhoncus feugiat ligula, et ultrices tortor efficitur vel. 
+                Etiam nisl arcu, scelerisque vitae nunc in, varius faucibus velit. Donec eleifend, quam sit amet lobortis scelerisque, felis nisi bibendum purus, in sodales mi ante quis libero.
+            </p>
+        </div>
+    </div>
+    
+    <div class="row mb-5">
+        <div class="col">
+            <p class="fs-3">Donec eget molestie eros.</p>
+            <p style="text-align: justify">Aliquam gravida egestas eleifend. Curabitur vitae rhoncus nisl, in sodales augue. Vivamus vulputate enim quam, at fringilla lectus fermentum vitae. 
+                Donec quis egestas mi, lacinia posuere risus. Proin eget ligula commodo augue fermentum aliquet eu ut orci. Nulla ornare ullamcorper risus a consequat. 
+                Curabitur ullamcorper volutpat leo at facilisis. Integer eget neque erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                Nullam consequat bibendum ante, non elementum arcu. Suspendisse ex magna, suscipit eget sollicitudin non, facilisis eget nibh. Ut malesuada nisi vitae blandit tincidunt. 
+                Sed vehicula dictum fringilla. Maecenas rutrum augue vel magna consectetur, eu mattis lacus luctus. Vestibulum iaculis porttitor mauris nec gravida.
+            </p>
+        </div>
+    </div>
+    
+    <h2 class="display-6 text-center mb-3">Chantiers du jour</h2>
 
-    <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget molestie eros. Aliquam sed enim eget leo lacinia pretium id sit amet mauris. Aenean sed pharetra massa. 
-        Vestibulum nibh erat, accumsan vitae elit in, semper commodo ipsum. Phasellus rhoncus feugiat ligula, et ultrices tortor efficitur vel. 
-        Etiam nisl arcu, scelerisque vitae nunc in, varius faucibus velit. Donec eleifend, quam sit amet lobortis scelerisque, felis nisi bibendum purus, in sodales mi ante quis libero.
-    </p>
-
-    <p style="text-align: justify">Aliquam gravida egestas eleifend. Curabitur vitae rhoncus nisl, in sodales augue. Vivamus vulputate enim quam, at fringilla lectus fermentum vitae. 
-        Donec quis egestas mi, lacinia posuere risus. Proin eget ligula commodo augue fermentum aliquet eu ut orci. Nulla ornare ullamcorper risus a consequat. 
-        Curabitur ullamcorper volutpat leo at facilisis. Integer eget neque erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-        Nullam consequat bibendum ante, non elementum arcu. Suspendisse ex magna, suscipit eget sollicitudin non, facilisis eget nibh. Ut malesuada nisi vitae blandit tincidunt. 
-        Sed vehicula dictum fringilla. Maecenas rutrum augue vel magna consectetur, eu mattis lacus luctus. Vestibulum iaculis porttitor mauris nec gravida.
-    </p>
-
-    <p style="text-align: justify">Quisque lorem eros, dictum in mauris a, viverra varius neque. Phasellus justo dui, posuere nec justo vitae, accumsan lobortis turpis. 
-        Suspendisse tincidunt consectetur nisl nec tincidunt. Proin vel viverra arcu. Quisque turpis arcu, posuere non maximus eget, suscipit eu lectus. 
-        Vivamus ipsum nisl, pretium id fermentum eu, mattis non est. Quisque scelerisque gravida euismod. Nullam luctus mi nec diam lobortis blandit. 
-        Quisque mattis molestie neque quis consectetur. Nullam dignissim quam sed congue posuere. Aliquam ante diam, congue non hendrerit non, tempor eu ante. 
-        Vestibulum turpis purus, eleifend non lobortis nec, egestas nec tortor. Morbi vel sollicitudin urna, eget ullamcorper augue.
-    </p>
-
-    <p style="text-align: justify">Morbi interdum semper arcu luctus sodales. Mauris scelerisque suscipit neque. Cras nec ex placerat magna vulputate tincidunt sit amet vitae nunc. 
-        Curabitur a dictum quam, sit amet fermentum ipsum. Quisque eget arcu eget nisl volutpat blandit a vitae nunc. Donec eu gravida nisi. 
-        Sed vitae orci vitae ex sodales venenatis.
-    </p>
-
-    <p style="text-align: justify">Aenean arcu ipsum, scelerisque et venenatis in, dapibus et nisi. Mauris mattis erat lectus, vel tincidunt tortor tempor a. 
-        In lacinia erat eget felis commodo volutpat. Suspendisse imperdiet ut urna a maximus. 
-        Aenean pharetra, tortor eu rhoncus dapibus, metus nulla tempor nisi, id condimentum massa velit vitae ex. 
-        Nam id egestas mi, semper tristique nunc. Aenean vitae ante eu nunc ullamcorper mattis. Fusce accumsan quis nisi vitae suscipit. 
-        Quisque vel metus posuere, euismod mi eu, porttitor quam.
-    </p>
+    <div class="row mb-5">
+        <div class="col mt-3">
+            <p class="fs-4 text-center" id="currentDate">JJ/MM/AAAA</p>
+        </div>
+    </div>
 
 <?php $content = ob_get_clean(); ?>
 
@@ -44,7 +45,16 @@ $heading = "Chantiers du jour";
     <script>
         window.onload = function() {
             getNumberOfRecordsToCheck('Check', 'unchecked');
+            getCurrentDate();
     }
+
+        function getCurrentDate() {
+            const today = new Date();
+            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+            currentDate = document.getElementById("currentDate");
+            currentDate.innerHTML = today.toLocaleDateString('fr-FR', options);
+        }
     </script>
 
 <?php $script = ob_get_clean(); ?>
