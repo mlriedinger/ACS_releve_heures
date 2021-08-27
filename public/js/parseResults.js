c866057b00c4821caa69cdd6a452068d94e11bcf
Marie-Laure Riedinger <ml.riedinger@acskm.fr>
2021-08-27 11:21:58 +0200

    refactor(ajax): delete unecessary function

--- public/js/parseResults.js
+++ public/js/parseResults.js
@@ -12,17 +12,3 @@ function parseMultipleLines(result) {
         }
     })
 }
-
-
-/** Traite les résultats renvoyés par un appel AJAX lorsque la requête renvoie une seule ligne, et les insère dans un tableau pour pré-remplir le formulaire lors de l'édition.
- * @param  {object} result
- */
-function parseUniqueLine(result) {
-    var recordData = [];
-
-    $.each(result, function(key, value) {
-        recordData.push(value);
-    });
-    
-    prefillRecordData(recordData);
-}
